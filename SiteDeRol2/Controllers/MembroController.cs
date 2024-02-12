dea0426ed638f7b501018bf74ed57cd0ab99049d
Whalisson Araujo <whalissonfelipe@hotmail.com>
2024-02-12 11:59:13 -0300

    Dia 1

---
+++ SiteDeRol2/Controllers/MembroController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SiteDeRol2.Controllers
+{
+    public class MembroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+
+        public IActionResult Detalhe()
+        {
+            return View();
+        }
+
+        public IActionResult Apagar()
+        {
+            return View();
+        }
+    }
+}
--- SiteDeRol2/obj/Debug/net8.0/SiteDeRol2.AssemblyInfo.cs
+++ SiteDeRol2/obj/Debug/net8.0/SiteDeRol2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SiteDeRol2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+accb24bd48bfb18c511d78d3642372ef6172d460")]
 [assembly: System.Reflection.AssemblyProductAttribute("SiteDeRol2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SiteDeRol2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
